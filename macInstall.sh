47340b421e0b83d2a2288af09264aaea5871ce07
wing-anemone <utidaaya@163.com>
2023-05-05 22:33:47 +0800

    update macinstall sh

--- macInstall.sh
+++ macInstall.sh
@@ -3,38 +3,39 @@
 # need to check fialed command
 
 set -uxo pipefail
+echo "the first time you run? [y/N]"
+read first
+[[ -z $first ]] && first="N"
+cd
 
-brew install zsh wget subversion alacritty zellij ripgrep fd-find
+brew install zsh wget subversion alacritty \
+  zellij ripgrep fd-find clang-format
 
-Config=$HOME/.config
+readonly Config=$HOME/.config
+readonly Alacritty=$Config/alacritty
+readonly Zellij=$Config/zellij
 
-Alacritty=$Config/alacritty
-echo $Alacritty
 if [[ ! -d $Alacritty ]]; then 
   mkdir -p $Alacritty
 fi
-# 如果存在软链，直覆盖
-ln -sf $HOME/myConfig/alacritty.yml $Alacritty/alacritty.yml
-
-Zellij=$Config/zellij
 if [[ ! -d $Zellij ]]; then 
   mkdir -p $Zellij
 fi
-ln -sf $HOME/myConfig/zellij.yaml $Zellij/config.yaml
 
-# 重复链接目录会有出现在后者目录里
+# 如果存在软链，直覆盖
+ln -sf $HOME/myConfig/alacritty.yml $Alacritty/alacritty.yml
+ln -sf $HOME/myConfig/zellij.kdl $Zellij/config.kdl
+ln -sf $HOME/myConfig/zshrc $HOME/.zshrc
+ln -sf $HOME/myConfig/nvim/vim/vimrc $HOME/.vimrc
+
+# 重复链接目录会出现在后者目录里
 if [[ -d $Config/nvim ]]; then
   rm $Config/nvim
 fi
 ln -s $HOME/myConfig/nvim $Config/nvim
-ln -sf $HOME/myConfig/zshrc $HOME/.zshrc
-ln -sf $HOME/myConfig/nvim/vim/vimrc $HOME/.vimrc
 
 # 第一次运行脚本，需要安装SauceCodePro Nerd Font字体
-if [[ -n $1 && $1 == "first" ]]; then
-  wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.1/SourceCodePro.zip
-  unzip SourceCodePro.zip -d ~/.fonts
-  rm SourceCodePro.zip
-  fc-cache -fv ~/.fonts
-  rm -r ~/.fonts
+if [[ $first == "y" || $first == "yes" ]]; then
+  brew tap homebrew/cask-fonts
+  brew install font-sauce-code-pro-nerd-font --cask
 fi 
